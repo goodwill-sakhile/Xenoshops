480b7271c4635b55154b010c381abd27b5f594fa
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-02 00:37:10 +0200

    urls.py Script

--- retail_backend/urls.py
+++ retail_backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import RegisterUser
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,5 @@ urlpatterns = [
     path('register/', RegisterUser.as_view()),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 router.register(r'reviews', ReviewViewSet)
