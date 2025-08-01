136cfbd2d8c69d812d1801368353aab41d156ed7
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-01 23:48:41 +0200

    views.py Script

---
+++ views.py
@@ -0,0 +1,14 @@
+from .models import Favorite
+from .serializers import FavoriteSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class FavoriteViewSet(viewsets.ModelViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
