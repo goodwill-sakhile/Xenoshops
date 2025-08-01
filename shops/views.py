3bc888231f4c7b8ae974ebc15582c15bc790c314
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-02 00:14:37 +0200

    views.py Script

--- shops/views.py
+++ shops/views.py
@@ -2,8 +2,20 @@ from rest_framework import viewsets, filters
 from .models import Shop
 from .serializers import ShopSerializer
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import status
 
-
+class RegisterUser(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Username already exists"}, status=400)
+        user = User.objects.create_user(username=username, password=password)
+        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all().order_by('-created_at')
     serializer_class = ShopSerializer
