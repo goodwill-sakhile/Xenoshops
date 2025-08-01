425cae4415963d3f2ed781e694f6af0725e8d1f9
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-01 23:59:07 +0200

    urls.py Script

--- shops/urls.py
+++ shops/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ShopViewSet
 from .views import FavoriteViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router.register(r'favorites', FavoriteViewSet)
 router = DefaultRouter()
@@ -9,4 +10,5 @@ router.register(r'shops', ShopViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', obtain_auth_token),
 ]
