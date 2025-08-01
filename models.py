57f4008b85b57a62da3d6d8c3171333041362051
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-02 00:34:37 +0200

    models.py Script

--- models.py
+++ models.py
@@ -4,6 +4,8 @@ class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='favorited_by')
     added_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='shop_images/', blank=True, null=True)
+
 
     class Meta:
         unique_together = ('user', 'shop')
