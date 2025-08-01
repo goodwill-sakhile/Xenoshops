bc86f76ccce1c61a64ab7167261df5e81a51d3e3
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-02 00:26:59 +0200

    urls.py Script

--- retail_backend/urls.py
+++ retail_backend/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shops.urls')),
     path('register/', RegisterUser.as_view()),
+
 ]
+router.register(r'reviews', ReviewViewSet)
