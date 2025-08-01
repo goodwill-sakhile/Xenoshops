44ae815602d698580f3e8b5b6873842f2752d06d
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-02 00:16:27 +0200

    urls.py Script

--- retail_backend/urls.py
+++ retail_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import RegisterUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shops.urls')),
+    path('register/', RegisterUser.as_view()),
 ]
