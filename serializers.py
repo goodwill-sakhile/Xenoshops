3a347c692f54ad1ae798f746af026e55b879ed3c
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-02 00:23:23 +0200

    serializers.py Script

--- serializers.py
+++ serializers.py
@@ -1,4 +1,10 @@
 from .models import Favorite
+from .models import Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
