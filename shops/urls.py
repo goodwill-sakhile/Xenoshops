f0319ffa745a40e52491136a23e79867879ee967
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-08-01 23:52:40 +0200

    urls.py Script

--- shops/urls.py
+++ shops/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ShopViewSet
+from .views import FavoriteViewSet
 
+router.register(r'favorites', FavoriteViewSet)
 router = DefaultRouter()
 router.register(r'shops', ShopViewSet)
 
